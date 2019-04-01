c52d4085685c6ddda50480e69ef9e42fec9814cd
Reilley Ford <reilleyford@gmail.com>
2019-04-01 11:12:46 -0400

    Overloaded filtering by property number

---
+++ ENTable.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Cain {
+    public class ENTable {
+        public List<ENTableRow> Rows { get; set; }
+
+        public ENTable() {
+            Rows = new List<ENTableRow>();
+        }
+
+        public DataTable ConvertToDataTable() {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Entry Number", typeof(string));
+            dt.Columns.Add("Entry Date Time", typeof(string));
+            dt.Columns.Add("Entry Info", typeof(string));
+
+            foreach (ENTableRow row in this.Rows) {
+                DataRow dr = dt.NewRow();
+                dr[0] = row.EntryNumber;
+                if (row.EntryDateTime.HasValue) {
+                    dr[1] = row.EntryDateTime.Value.ToString("dd MMM yyyy HH:mm");
+                }
+                dr[2] = row.EntryContent;
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+
+        public DateTime GetStartTime() {
+            DateTime start = new DateTime();
+            foreach(ENTableRow first in this.Rows) {
+                if (first.EntryDateTime != null) {
+                    start = (DateTime)first.EntryDateTime;
+                    break;
+                }
+            }
+            return start;
+        }
+
+        public DateTime GetEndTime() {
+            DateTime end = (DateTime)this.Rows.Last().EntryDateTime;
+            return end;
+        }
+
+        public List<ENTableRow> FilterByProperty(string propertyNum) {
+            List<ENTableRow> rows = new List<ENTableRow>();
+            string pattern = @"^P\d{8}";
+            Regex rgx = new Regex(pattern);
+
+            foreach (ENTableRow row in this.Rows) {
+                if (rgx.Match(row.EntryContent.Substring(0, 9)).ToString() == propertyNum) 
+                    rows.Add(row);
+            }
+
+            return rows;
+        }
+
+        public List<FilteredRow> FilterByProperty(List<string> propertyNums) {
+            List<FilteredRow> filteredRows = new List<FilteredRow>();
+
+            string pattern = @"^P\d{8}";
+            Regex rgx = new Regex(pattern);
+            foreach(string pNum in propertyNums) {
+                FilteredRow filteredRow = new FilteredRow();
+                List<ENTableRow> newRow = new List<ENTableRow>();
+                filteredRow.Rows = newRow;
+                foreach (ENTableRow row in this.Rows) {
+                    string match = rgx.Match(row.EntryContent.Substring(0, 9)).ToString();
+                    if (match == pNum) {
+                        filteredRow.PropertyNumber = match;
+                        filteredRow.Rows.Add(row);
+                    }
+                }
+                filteredRows.Add(filteredRow);
+            }
+
+
+            return filteredRows;
+        }
+
+        public List<string> GetDistinctPropertyNumber() {
+            List<string> propertyNums = new List<string>();
+            string pattern = @"^P\d{8}";
+            Regex rgx = new Regex(pattern);
+            foreach (ENTableRow row in this.Rows) {
+                if (rgx.IsMatch(row.EntryContent))
+                    propertyNums.Add(row.EntryContent.Substring(0, 9));
+            }
+
+            propertyNums = propertyNums.Distinct().OrderBy(p => p).ToList();
+            return propertyNums;
+        }
+
+    }   
+
+    public class ENTableRow {
+        public string EntryNumber { get; set; }
+        public Nullable<DateTime> EntryDateTime { get; set; }
+        public string EntryContent { get; set; }
+    }
+
+    public class FilteredRow
+    {
+        public string PropertyNumber { get; set; }
+        public List<ENTableRow> Rows { get; set; }
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Text.RegularExpressions;
+
+namespace Cain {
+    public static class CainLibrary {
+
+        public static ENTable ConvertDocxToENTable(string @path) {
+            ENTable enTable = new ENTable();
+
+            using (WordprocessingDocument docx = 
+                WordprocessingDocument.Open(path, false)) {
+
+                MainDocumentPart main = docx.MainDocumentPart;
+                //IEnumerable<HeaderPart> headerParts = main.HeaderParts;
+                //HeaderPart header = headerParts;
+
+                Table table = main.Document.Body.Elements<Table>().First();
+                string date = "";
+                int num = 0, time = 1, content = 2;
+
+                // Definite work in progress, REQUIRES REWORK.
+                foreach (TableRow row in table.Elements<TableRow>()) { 
+                    if(row.Count() == 4) { num = 1; time = 2; content = 3; }
+
+                    if(row.ElementAt(2).InnerText != "") {  
+                        ENTableRow newRow = new ENTableRow();
+                        Regex rgx = new Regex(@"(?m)\d{3}$");
+
+                        string entryNum = row.ElementAt(num).InnerText;
+                        newRow.EntryNumber = rgx.Match(entryNum).ToString();
+
+                        rgx = new Regex(@"^\d{2}\w{3}\d{4}");
+                        date = rgx.Match(entryNum).ToString();
+
+                        rgx = new Regex(@"^\d{4}");
+                        string timeStamp = row.ElementAt(time).InnerText;
+                        if (rgx.IsMatch(timeStamp)) {
+                            string dateTime = date + " " + timeStamp.Insert(2, ":");
+                            newRow.EntryDateTime = Convert.ToDateTime(dateTime.Trim());
+                        } else
+                            newRow.EntryDateTime = null;
+
+                        newRow.EntryContent = row.ElementAt(content).InnerText;
+                        enTable.Rows.Add(newRow);
+                    }
+                }
+            }
+            return enTable;
+        }
+
+        
+    }
+}
