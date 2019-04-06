374ee9efbb6ef6d3b57fd69af95292c6ca9b724f
ReilleyFord <reilleyford@gmail.com>
2019-04-06 16:09:38 -0400

    restructured project

---
+++ Cain/Classes/Case.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cain {
+    public class Case {
+        public string          RootPath  { get; set; }
+        public List<CaseNotes> CaseNotes { get; set; }
+        public CaseDirectory   CaseDirectory  { get; set; }
+
+        public Case() {
+            CaseNotes = new List<CaseNotes>();
+        }
+
+        public Case(string path, List<CaseNotes> caseNotes, CaseDirectory caseDirectory) {
+            this.RootPath      = path;
+            this.CaseNotes     = caseNotes;
+            this.CaseDirectory = caseDirectory;
+        }
+    }
+}
