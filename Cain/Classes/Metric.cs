bce1417d4ae9f521510b985a736efb5601104ce1
Reilley Ford <reilleyford@gmail.com>
2019-04-12 15:02:01 -0400

    iT wOrKs On mY mAcHiNe

--- Cain/Classes/Metric.cs
+++ Cain/Classes/Metric.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.Threading;
 
 namespace Cain
 {
+    public enum MetricType { Case, Property }
+
     public class Metric
     {
-        public int    Id            { get; set; }
-        public string Name          { get; set; }
-        public string StartingValue { get; set; }
-        public string EndingValue   { get; set; }
+        public int        Id            { get; set; }
+        public string     Name          { get; set; }
+        public string     StartingValue { get; set; }
+        public string     EndingValue   { get; set; }
+        public MetricType Type          { get; set; }
 
-        public Metric() {
+        public Metric() { }
 
-        }
-
-        public Metric(string name, string start, string end) {
-            this.Name = name;
+        public Metric(string name, string start, string end, MetricType type) {
+            this.Name          = name;
             this.StartingValue = start;
-            this.EndingValue = end;
+            this.EndingValue   = end;
+            this.Type          = type;
         }
+
     }
 }
--- Cain/Classes/MetricFile.cs
+++ Cain/Classes/MetricFile.cs
@@ -4,28 +4,26 @@ using System.IO;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace Cain
 {
     public class MetricFile
     {
- 
+        private int          Id      { get; set; }
         private List<Metric> Metrics { get; set; }
         private string       Path    { get; set; }
         private string       File    { get; set; }
         private JObject      JSON    { get; set; }
-        private int          Id      { get; set; }
 
         public MetricFile(string path) {
             this.Path    = path;
             this.Metrics = new List<Metric>();
-
             if (System.IO.File.Exists(path))
                 LoadMetricsFile(path);
             else
                 CreateMetricsFile(path);
 
-            this.Id = GetCurrentId();
             LoadMetrics();
         }
 
@@ -34,7 +32,7 @@ namespace Cain
 
             JArray  metrics = new JArray();
             JObject obj     = new JObject();
-            obj["Metrics"] = metrics;
+            obj["Metrics"]  = metrics;
 
             System.IO.File.WriteAllText(this.Path, JsonConvert.SerializeObject(obj));
             LoadMetricsFile(path);
@@ -48,22 +46,22 @@ namespace Cain
 
         }
 
-        private void LoadMetrics() { 
+        private void LoadMetrics() {
+            List<Metric> metrics = new List<Metric>();
             foreach(JToken metric in this.JSON.GetValue("Metrics")) {
                 Metric newMetric = new Metric();
                 newMetric.Id = (int)metric["id"];
                 newMetric.Name = (string) metric["Name"];
                 newMetric.StartingValue = (string) metric["Start Value"];
                 newMetric.EndingValue = (string) metric["End Value"];
+                if ((string)metric["Type"] == MetricType.Case.ToString())
+                    newMetric.Type = MetricType.Case;
+                else if ((string)metric["Type"] == MetricType.Property.ToString())
+                    newMetric.Type = MetricType.Property;
 
-                this.Metrics.Add(newMetric);
+                metrics.Add(newMetric);
             }
-            this.Id = GetCurrentId();
-        }
-
-        public List<Metric> GetMetrics() {
-            LoadMetrics();
-            return this.Metrics;
+            this.Metrics = metrics;
         }
 
         private JObject BuildJObject(Metric metric) {
@@ -71,28 +69,28 @@ namespace Cain
                 new JProperty("id", this.Id),
                 new JProperty("Name", metric.Name),
                 new JProperty("Start Value", metric.StartingValue),
-                new JProperty("End Value", metric.EndingValue)
+                new JProperty("End Value", metric.EndingValue),
+                new JProperty("Type", metric.Type)
                 );
             return jObj;
         }
 
-        public int GetCurrentId() {
-            if (this.Id == 0)
-                return this.Id;
-            else {
-                JArray metrics = (JArray)this.JSON["Metrics"];
-                JToken lastMetric = metrics.Last;
-
-                int id = (int)lastMetric["id"];
-
-                return id;
-            }
+        public List<Metric> GetMetrics() {
+            LoadMetrics();
+            return this.Metrics;
         }
 
-        public void CreateMetric(string name, string startingValue, string endingValue) {
-            this.Id++;
-            Metric metric = new Metric(name, startingValue, endingValue);
-            metric.Id = this.Id;
+        public void SetId() {
+            LoadMetrics();
+            if (this.Metrics.Count == 0)
+                return;
+            int id = this.Metrics.Last().Id;
+            this.Id = id + 1;
+        }
+
+        public void CreateMetric(string name, string startingValue, string endingValue, MetricType type) {
+            SetId();
+            Metric metric = new Metric(name, startingValue, endingValue, type);
 
             JArray metrics = (JArray)this.JSON["Metrics"];
             JObject newMetric = BuildJObject(metric);
@@ -104,7 +102,7 @@ namespace Cain
         }
 
         public void CreateMetric(Metric metric) {
-            this.Id++;
+            SetId();
             JArray metrics = (JArray)this.JSON["Metrics"];
             JObject newMetric = BuildJObject(metric);
             metrics.Add(newMetric);
@@ -116,24 +114,14 @@ namespace Cain
         }
 
         public Metric GetMetric(int id) {
-            Metric metric = new Metric();
-
-            try {
-                foreach (Metric m in this.Metrics) {
-                    if (m.Id == id)
-                        metric = m;
-                    else
-                        throw new System.ArgumentOutOfRangeException("Error: That ID was not found");
-                }
-            } catch (System.ArgumentOutOfRangeException e) { Console.WriteLine(e.Message); }
-
-            return metric;
+            throw new NotImplementedException();
+            //return metric;
         }
 
-        public void UpdateMetric(int id, string name, string startingValue, string endingValue) {
+        public void UpdateMetric(int id, string name, string startingValue, string endingValue, MetricType type) {
             JArray metrics = (JArray)this.JSON["Metrics"];
             JObject oldMetric = new JObject();
-            Metric metric = new Metric(name, startingValue, endingValue);
+            Metric metric = new Metric(name, startingValue, endingValue, type);
 
             JObject newMetric = BuildJObject(metric);
             try {
@@ -141,9 +129,26 @@ namespace Cain
                     if ((int)m["id"] == id)
                         oldMetric = m;
                     else
-                        throw new System.ArgumentOutOfRangeException("Error: That ID was not found");
+                        throw new ArgumentOutOfRangeException("Error: That ID was not found");
                 }
-            } catch(System.ArgumentOutOfRangeException e) { Console.WriteLine(e.Message); }
+            } catch(ArgumentOutOfRangeException e) { Console.WriteLine(e.Message); }
+
+            oldMetric.Replace(newMetric);
+            string newJson = JsonConvert.SerializeObject(this.JSON,
+                   Formatting.Indented);
+            System.IO.File.WriteAllText(this.Path, newJson);
+            LoadMetrics();
+        }
+
+        public void UpdateMetric(Metric metric) {
+            JArray metrics = new JArray(this.Metrics);
+            JObject oldMetric = new JObject();
+
+            JObject newMetric = BuildJObject(metric);
+            foreach (JObject m in metrics) {
+                if ((int)m["id"] == metric.Id)
+                    oldMetric = m;
+            }
 
             oldMetric.Replace(newMetric);
             string newJson = JsonConvert.SerializeObject(this.JSON,
@@ -153,7 +158,7 @@ namespace Cain
         }
 
         public void DeleteMetric(int id) {
-            JArray metrics = (JArray)this.JSON["Metrics"];
+            JArray metrics = new JArray(this.Metrics); //) metrics.ToObject<List<Metric>>();
             JObject oldMetric = new JObject();
 
             try {
@@ -161,9 +166,29 @@ namespace Cain
                     if ((int)m["id"] == id)
                         oldMetric = m;
                     else
-                        throw new System.ArgumentOutOfRangeException("Error: That ID was not found");
+                        throw new ArgumentOutOfRangeException("Error: That ID was not found");
                 }
-            } catch (System.ArgumentOutOfRangeException e) { Console.WriteLine(e.Message); }
+            } catch (ArgumentOutOfRangeException e) { Console.WriteLine(e.Message); }
+
+            metrics.Remove(oldMetric);
+            string newJson = JsonConvert.SerializeObject(this.JSON,
+                   Formatting.Indented);
+            System.IO.File.WriteAllText(this.Path, newJson);
+            LoadMetrics();
+        }
+
+        public void DeleteMetric(Metric metric) {
+            JArray metrics = new JArray(this.Metrics); //) metrics.ToObject<List<Metric>>();
+            JObject oldMetric = new JObject();
+
+            try {
+                foreach (JObject m in metrics) {
+                    if ((int)m["id"] == metric.Id)
+                        oldMetric = m;
+                    else
+                        throw new ArgumentOutOfRangeException("Error: That ID was not found");
+                }
+            } catch (ArgumentOutOfRangeException e) { Console.WriteLine(e.Message); }
 
             metrics.Remove(oldMetric);
             string newJson = JsonConvert.SerializeObject(this.JSON,
